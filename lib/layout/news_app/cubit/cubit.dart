ebbd2abcfa944c2a704e3d8c4640d89f6da3348e
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-13 04:44:03 +0200

    add new module Settings Screen

--- lib/layout/news_app/cubit/cubit.dart
+++ lib/layout/news_app/cubit/cubit.dart
@@ -1,9 +1,47 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/layout/news_app/cubit/states.dart';
+import 'package:news_app/modules/business/business_screen.dart';
+import 'package:news_app/modules/sciences/sciences_screen.dart';
+import 'package:news_app/modules/settings/settings_screen.dart';
+import 'package:news_app/modules/sports/sports_screen.dart';
 
 class NewsCubit extends Cubit<NewsStates> {
   NewsCubit() : super(NewsInitialStates());
 
   static NewsCubit get(context) => BlocProvider.of(context);
+
+  int currentIndex = 0;
+
+  List<BottomNavigationBarItem> bottomItems = [
+    const BottomNavigationBarItem(
+        icon: Icon(Icons.business), label: 'Business'),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.sports),
+      label: 'Sports',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.science),
+      label: 'Sciences',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.settings),
+      label: 'Settings',
+    ),
+  ];
+
+//this method for change location in bottom nav bar
+  void changeBottomNavBar(int index) {
+    currentIndex = index;
+    emit(NewsBottomNavStates());
+  }
+
+  //list for screens
+  List<Widget> screens = [
+    const BusinessScreen(),
+    const SportsScreen(),
+    const SciencesScreen(),
+    const SettingsScreen(),
+  ];
 }
