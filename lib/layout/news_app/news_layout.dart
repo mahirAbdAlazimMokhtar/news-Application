aaa6e7a1b9e40134c72ea90e3751f1a2d976d419
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-13 04:44:55 +0200

    add cubit and blocConsumer to news screen and handled the screen

--- lib/layout/news_app/news_layout.dart
+++ lib/layout/news_app/news_layout.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/layout/news_app/cubit/cubit.dart';
+import 'package:news_app/layout/news_app/cubit/states.dart';
 
 class NewsLayout extends StatelessWidget {
   const NewsLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (BuildContext context) => NewsCubit(),
+      child: BlocConsumer<NewsCubit, NewsStates>(
+        builder: (context, states) {
+          NewsCubit cubit = NewsCubit.get(context);
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('News App'),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (index) {
+                cubit.changeBottomNavBar(index);
+              },
+              items: cubit.bottomItems,
+              currentIndex: cubit.currentIndex,
+            ),
+            body: cubit.screens[cubit.currentIndex],
+          );
+        },
+        listener: (context, states) {},
+      ),
+    );
   }
 }
