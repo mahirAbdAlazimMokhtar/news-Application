916022a8c65529843879176f5efd05de89e825af
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-13 03:27:22 +0200

    add 3 Modules Business Screen , Sport Screen and Sciences Screen

---
+++ lib/modules/science/scinece_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SciencesScreen extends StatelessWidget {
+  const SciencesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Sciences Screen',
+        style: TextStyle(fontSize: 30.0),
+      ),
+    );
+  }
+}
