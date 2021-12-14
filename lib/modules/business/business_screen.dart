fe1edf013f00414ed5f545ee6f6885bf943cee82
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-14 08:38:19 +0200

    customize and add cubit to business screen

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/layout/news_app/cubit/cubit.dart';
+import 'package:news_app/layout/news_app/cubit/states.dart';
+import 'package:news_app/shared/components/components_screen.dart';
 
 class BusinessScreen extends StatelessWidget {
   const BusinessScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Business Screen',
-        style: TextStyle(fontSize: 30.0),
-      ),
-    );
+    return BlocConsumer<NewsCubit, NewsStates>(
+        builder: (context, states) {
+          var list = NewsCubit.get(context).business;
+          if (states is! NewsGetBusinessLoadingStates) {
+            return ListView.separated(
+                //this 4 active scroll for screen
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return buildArticleItem(list[index]);
+                },
+                separatorBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Container(
+                      height: 1.0,
+                      width: double.infinity,
+                      color: Colors.grey,
+                    ),
+                  );
+                },
+                itemCount: 10);
+          } else {
+            return const Center(
+                child: CircularProgressIndicator(
+              color: Colors.deepOrange,
+              backgroundColor: Colors.black,
+            ));
+          }
+        },
+        listener: (context, states) {});
   }
 }
