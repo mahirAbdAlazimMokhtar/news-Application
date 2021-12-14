28371dc36c680626ccdfdd4ee352ac26b30938b9
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-14 08:41:18 +0200

    add customize widget to screen

--- lib/layout/news_app/news_layout.dart
+++ lib/layout/news_app/news_layout.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/layout/news_app/cubit/cubit.dart';
@@ -9,7 +10,7 @@ class NewsLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (BuildContext context) => NewsCubit(),
+      create: (BuildContext context) => NewsCubit()..getBusiness(),
       child: BlocConsumer<NewsCubit, NewsStates>(
         builder: (context, states) {
           NewsCubit cubit = NewsCubit.get(context);
