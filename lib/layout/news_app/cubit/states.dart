72eb81e3303de0cfe355f4c69eeda8c149447166
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-14 08:41:43 +0200

    add some states to states

--- lib/layout/news_app/cubit/states.dart
+++ lib/layout/news_app/cubit/states.dart
@@ -3,3 +3,33 @@ abstract class NewsStates {}
 class NewsInitialStates extends NewsStates {}
 
 class NewsBottomNavStates extends NewsStates {}
+
+//this for business states
+class NewsGetBusinessLoadingStates extends NewsStates {}
+
+class NewsGetBusinessDataSuccessStates extends NewsStates {}
+
+class NewsGetBusinessDataErrorStates extends NewsStates {
+  final String? error;
+  NewsGetBusinessDataErrorStates(this.error);
+}
+
+//this for sports states
+class NewsGetSportsLoadingStates extends NewsStates {}
+
+class NewsGetSportsDataSuccessStates extends NewsStates {}
+
+class NewsGetSportsDataErrorStates extends NewsStates {
+  final String? error;
+  NewsGetSportsDataErrorStates(this.error);
+}
+
+//this for sciences
+class NewsGetSciencesLoadingStates extends NewsStates {}
+
+class NewsGetSciencesDataSuccessStates extends NewsStates {}
+
+class NewsGetSciencesDataErrorStates extends NewsStates {
+  final String? error;
+  NewsGetSciencesDataErrorStates(this.error);
+}
