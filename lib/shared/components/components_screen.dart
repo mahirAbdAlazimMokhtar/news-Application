d3d7cdd2b98eb0aac56ccf7a1df4c08126e40319
Mahir Abd Alazim Mokhtar <mahr.bwb@gmail.com>
2021-12-15 11:57:42 +0200

    replace regular image by cachedNetworkImage to resolve null image

--- lib/shared/components/components_screen.dart
+++ lib/shared/components/components_screen.dart
@@ -1,22 +1,20 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 
-Widget buildArticleItem(article) {
+Widget buildArticleItem(article, context) {
   return Padding(
     padding: const EdgeInsets.all(20.0),
     child: Row(
       children: [
-        Container(
+        CachedNetworkImage(
           height: 120.0,
           width: 120.0,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  '${article['urlToImage']}',
-                ),
-              )),
+          fit: BoxFit.cover,
+          imageUrl: '${article['urlToImage']}',
+          placeholder: (context, url) => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
         const SizedBox(
           width: 20.0,
@@ -32,17 +30,12 @@ Widget buildArticleItem(article) {
                   child: Text(
                     '${article['title']}',
                     maxLines: 3,
-                    style: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                        overflow: TextOverflow.ellipsis),
+                    style: Theme.of(context).textTheme.bodyText1,
                   ),
                 ),
                 Text(
                   '${article['publishedAt']}',
-                  style: const TextStyle(
-                    fontSize: 18.0,
-                  ),
+                  style: Theme.of(context).textTheme.bodyText1,
                   maxLines: 3,
                   overflow: TextOverflow.ellipsis,
                 ),
